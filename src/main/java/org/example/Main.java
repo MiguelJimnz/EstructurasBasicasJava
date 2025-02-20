78f42016b449d86e2dc7227298c03119625d3a9c
MiguelJimenez <migl.jimnz55@gmail.com>
2025-02-20 12:34:30 -0500

    Variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //variables Enteras
+        byte numero1=120;
+        System.out.println("Variables byte "+numero1);
+
+        short numero2=30400;
+        System.out.println("Variable short "+numero2);
+
+
+
+        int numero3 = 234;
+        System.out.println("Variable int"+ numero3);
+        long numero4= 1231L;
+        System.out.println("variable long"+numero4);
+
+        //variables decimales
+
+        float decimal1 = 23.5f;
+        System.out.println("variable float"+decimal1);
+        double decimal2 = 123.6;
+        System.out.println("variable double"+ decimal2);
+
+
+
+        //variables String Cadena de caracteres
+
+        String nombre = "Miguel Angel Jimenez";
+        System.out.println("Variable cadena de caracteres "+ nombre);
+
+        //variables caracter
+
+        char letra ='a';
+        char letra1 = '65';
+        System.out.println("Variable cadena de char "+ letra);
+        System.out.println("Variable cadena de char "+ letra1);
+
+        //variable booleano
+        boolean estado = true;
+        System.out.println("Variable cadena de boolenao "+ estado);
+
+
+    }
+}
